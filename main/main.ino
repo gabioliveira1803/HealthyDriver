d1635509441e880731ae9e19da41e41d97223364
matheus.martins <matheus.martins@locaweb.com.br>
2019-09-17 09:58:36 -0300

    Add some comments.

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,18 @@
+//inserir lib do ble
+//verficar se nao precisa de lib para o sensor
+
 #define threshold 2400
 int a1 = 0;
 int bpm = 0;
 
 void setup() {
   Serial.begin(115200);
+  //iniciar ble
 }
 
 void loop() {
   bpm = analogRead(a1);                     
   Serial.println(bpm);                   
-  delay(1500); 
+  delay(1500);
+  //enviar bpm por bluetooth
 }
