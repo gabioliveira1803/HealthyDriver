f3f99db8f0d81c94de061016d4b77a1c0d353317
matheus.martins <matheus.martins@locaweb.com.br>
2019-09-26 19:22:42 -0300

    Change sensor to max 30100.

--- main/main.ino
+++ main/main.ino
@@ -1,27 +1,68 @@
-#define USE_ARDUINO_INTERRUPTS false
-#include <PulseSensorPlayground.h>
-#define PulseWire = 0; //this pin #12 on flora
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
 
-int Threshold = 550;
+#define REPORTING_PERIOD_MS 1000
+#define URI "http://jsonplaceholder.typicode.com/posts"
 
-PulseSensorPlayground pulseSensor;
+const char* ssid = "Meu 4G";
+const char* password = "Sl@yper91";
+
+int bpm = 0;
+
+PulseOximeter pox;
+HTTPClient http;
+
+void onBeatDetected() {
+  // For bpm, a value of 0 means "invalid"
+  bpm = pox.getHeartRate();
+  if (WiFi.status() == WL_CONNECTED) {
+    http.begin(URI);
+    http.addHeader("Content-Type", "text/plain");
+    int httpStatusCode = http.POST("POSTING from ESP32");
+    if (httpStatusCode > 0) {
+      String response = http.getString();
+      Serial.println(httpStatusCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpStatusCode);
+    }
+    http.end();
+  } else {
+    Serial.println("Error in WiFi connection");
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("cheguei ate aqui");
-  pulseSensor.analogInput(PulseWire);
-  //pulseSensor.blinkOnPulse(LED13);
-  pulseSensor.setThreshold(Threshold);
-  if (pulseSensor.begin()) {
-    Serial.println("We created a pulseSensor Object !");
+  Serial.print("Initializing pulse oximeter..");
+  // Initialize the PulseOximeter instance
+  // Failures are generally due to an improper I2C wiring, missing power supply
+  // or wrong target chip
+  if (!pox.begin()) {
+    Serial.println("FAILED");
+    for (;;); // loop infinito estranho
+  } else {
+    Serial.println("SUCCESS");
   }
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Connected to the WiFi network");
+  // The default current for the IR LED is 50mA and it could be changed
+  //   by uncommenting the following line. Check MAX30100_Registers.h for all the
+  //   available options.
+  // pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+  pox.setOnBeatDetectedCallback(onBeatDetected);
 }
+
 void loop() {
-  int myBPM = pulseSensor.getBeatsPerMinute();
-  if (pulseSensor.sawStartOfBeat()) {
-    Serial.println("♥ A HeartBeat Happened ! ");
-    Serial.print("BPM: ");
-    Serial.println(myBPM);
-  }
-  delay(1000);
+  pox.update();
 }
