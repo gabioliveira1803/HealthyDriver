34be3f1550e3caab0849915fc93d73614986ad78
Matheus A.M <matheusthebr@gmail.com>
2019-10-03 01:11:25 -0300

    Organize structure.

--- main/main.ino
+++ main/main.ino
@@ -6,8 +6,12 @@
 #define REPORTING_PERIOD_MS 1000
 #define URI "http://jsonplaceholder.typicode.com/posts"
 
-const char* ssid = "Meu 4G";
-const char* password = "Sl@yper91";
+IPAddress ip;
+
+uint32_t tsLastReport = 0;
+
+const char* ssid = "Sai da minha rede";
+const char* password = "Denise01Felipe02Gabi03";
 
 int bpm = 0;
 
@@ -31,38 +35,55 @@ void onBeatDetected() {
     }
     http.end();
   } else {
-    Serial.println("Error in WiFi connection");
+    Serial.println("Erro na conexão WiFi...");
   }
+  Serial.println("Batimento Detectado!");
 }
 
 void setup() {
   Serial.begin(115200);
-  Serial.print("Initializing pulse oximeter..");
+  Serial.print("Inicializando sensor de pulso...");
   // Initialize the PulseOximeter instance
   // Failures are generally due to an improper I2C wiring, missing power supply
   // or wrong target chip
   if (!pox.begin()) {
-    Serial.println("FAILED");
+    Serial.println("FALHOU!!");
     for (;;); // loop infinito estranho
   } else {
-    Serial.println("SUCCESS");
+    Serial.println("SUCESSO...");
   }
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+    delay(3000);
+    Serial.println("CONECTANDO AO WIFI...");
   }
 
-  Serial.println("Connected to the WiFi network");
+  Serial.println("CONECTADO A REDE WIFI!!");
   // The default current for the IR LED is 50mA and it could be changed
   //   by uncommenting the following line. Check MAX30100_Registers.h for all the
   //   available options.
   // pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
   pox.setOnBeatDetectedCallback(onBeatDetected);
+
+  ip = WiFi.localIP();
+  Serial.println(ip);
 }
 
-void loop() {
+void loop()
+{
+  // Make sure to call update as fast as possible
   pox.update();
+
+  // Asynchronously dump heart rate and oxidation levels to the serial
+  // For both, a value of 0 means "invalid"
+  if (millis() - tsLastReport > REPORTING_PERIOD_MS)
+  {
+    Serial.print("Batimento Cardiaco: ");
+    Serial.print(pox.getHeartRate());
+    Serial.print("bpm\n");
+    tsLastReport = millis();
+  }
+ //delay(2000);
 }
